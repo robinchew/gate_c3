8cbfda50fe7abbfd9902eb2c90a51d95f23bb801
Robin Chew <me@robin.com.au>
2025-11-24 23:32:27 +0800

    Connect to WIFI, send HTTP request on input wake up from deep sleep.

--- src/main.cpp
+++ src/main.cpp
@@ -1,91 +1,39 @@
 #include <WiFi.h>
-#include <BLEDevice.h>
-#include <BLEUtils.h>
-#include <BLEServer.h>
-#include <BLE2902.h> // Needed for the "Notify" feature
+#include <HTTPClient.h>
 
-#define SERVICE_UUID        "2f49436e-3900-44ea-8837-8843708d9ef0"
-#define CHARACTERISTIC_UUID "d2cdedc3-53a3-4a0a-adc7-2b2bf7e757f3"
+#define INPUT_PIN_D0 2
+#define INPUT_PIN_D1 3
 
-const int INPUT_PIN_D1 = 3;
+const int GATE_CLOSE_PIN = INPUT_PIN_D0;
+const char* ssid = "TPG-EYVZ-B";
+const char* password = "???";
 
-BLECharacteristic *pCharacteristic;
-bool deviceConnected = false;
-
-// Callback to handle connections/disconnections
-class MyServerCallbacks: public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
-      deviceConnected = true;
-      Serial.println("Device Connected");
-      
-    };
-
-    void onDisconnect(BLEServer* pServer) {
-      deviceConnected = false;
-      Serial.println("Device Disconnected");
-      // Restart advertising so we can connect again without rebooting
-      BLEDevice::startAdvertising();
-    }
-};
+void send(HTTPClient &http, String path) {
+    http.begin("http://192.168.1.13:8080/" + path);
+    http.GET();
+    http.end();
+}
 
 void setup() {
-  Serial.begin(115200);
+    Serial.begin(115200); // This is required for print to work
+    delay(1000);
 
-  // Small delay to allow Serial to initialize before printing
-  delay(1000);
+    pinMode(GATE_CLOSE_PIN, INPUT_PULLUP);  // Internal pull-up
+    esp_deep_sleep_enable_gpio_wakeup(1 << GATE_CLOSE_PIN, ESP_GPIO_WAKEUP_GPIO_LOW);
 
-  pinMode(INPUT_PIN_D1, INPUT_PULLUP);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
 
-  // Create the message
-  int currentState = digitalRead(INPUT_PIN_D1);
+    HTTPClient http;
+    send(http, "close");
+    send(http, "not-close");
 
-  // 3. Determine the NEXT wake-up logic
-  // If pin is currently HIGH, we want to wake up when it goes LOW.
-  // If pin is currently LOW, we want to wake up when it goes HIGH.
-  esp_deepsleep_gpio_wake_up_mode_t wakeupMode;
-
-  if (currentState == 1) {
-    Serial.println("Configuring wake-up for LOW state...");
-    wakeupMode = ESP_GPIO_WAKEUP_GPIO_LOW;
-  } else {
-    Serial.println("Configuring wake-up for HIGH state...");
-    wakeupMode = ESP_GPIO_WAKEUP_GPIO_HIGH;
-  }
-
-  // Enable the wake-up source
-  // The first argument is a bitmask of the pin ((1ULL << pin_number))
-  esp_err_t err = esp_deep_sleep_enable_gpio_wakeup(1ULL << INPUT_PIN_D1, wakeupMode);
-  if (err != ESP_OK) {
-    Serial.printf("Error enabling GPIO wakeup: %d\n", err);
-  }
-    Serial.println("blah");
-
-  const char* err_msg = (err != ESP_OK) ? "ERR_SLEEP"  : "OK";
-
-  const char* status = (currentState == 1) ? "HIGH" : "LOW";
-  char message[22];
-
-    Serial.println("blah1");
-  snprintf(message, sizeof(message), "BLE = %s, %s", status, err_msg);
-    Serial.println("blah2");
-  
-  // Update the value
-  // pCharacteristic->setValue(message);
-  
-  // Send the notification (Push the data to the phone/PC)
-  // pCharacteristic->notify();
-  
-  // Serial.println("Notification sent");
-  // go back to sleep
-  //   Serial.println("Entering Deep Sleep now...");
-  Serial.flush(); // Ensure all serial data is sent before sleeping
-
-  // 5. Start Deep Sleep
-  esp_deep_sleep_start();
+    Serial.println("sleep");
+    Serial.flush();
+    esp_deep_sleep_start();
 }
 
-void loop() {
-  // Only send data if something is actually connected
-  if (deviceConnected) {
-  }
-}
+void loop() {}
